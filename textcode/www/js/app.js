a8374f050cc6391484dd49a5fe3855dc908e61d7
inndee <inndeemercado@hotmail.com>
2016-04-14 21:27:41 +0800

    initial coding

--- textcode/www/js/app.js
+++ textcode/www/js/app.js
@@ -4,7 +4,7 @@
 // 'starter' is the name of this angular module example (also set in a <body> attribute in index.html)
 // the 2nd parameter is an array of 'requires'
 // 'starter.controllers' is found in controllers.js
-angular.module('starter', ['ionic', 'starter.controllers'])
+angular.module('textcode', ['ionic', 'textcode.controllers'])
 
 .run(function($ionicPlatform) {
   $ionicPlatform.ready(function() {
@@ -59,6 +59,26 @@ angular.module('starter', ['ionic', 'starter.controllers'])
       }
     })
 
+    .state('app.mobile', {
+      url: '/mobile/:grouplistsId',
+      views: {
+        'menuContent': {
+          templateUrl: 'templates/mobile.html',
+          controller: 'MobileCtrl'
+        }
+      }
+    })
+
+     .state('app.front', {
+      url: '/front',
+      views: {
+        'menuContent': {
+          templateUrl: 'templates/front.html',
+          controller: 'FrontPageCtrl'
+        }
+      }
+    })
+
   .state('app.single', {
     url: '/playlists/:playlistId',
     views: {
@@ -69,5 +89,5 @@ angular.module('starter', ['ionic', 'starter.controllers'])
     }
   });
   // if none of the above states are matched, use this as the fallback
-  $urlRouterProvider.otherwise('/app/playlists');
+  $urlRouterProvider.otherwise('/app/front');
 });
--- textcode/www/js/controllers.js
+++ textcode/www/js/controllers.js
@@ -1,56 +1,119 @@
-angular.module('starter.controllers', [])
+(function() {
 
-.controller('AppCtrl', function($scope, $ionicModal, $timeout) {
+var app = angular.module('textcode.controllers', []);
 
-  // With the new view caching in Ionic, Controllers are only called
-  // when they are recreated or on app start, instead of every page change.
-  // To listen for when this page is active (for example, to refresh data),
-  // listen for the $ionicView.enter event:
-  //$scope.$on('$ionicView.enter', function(e) {
-  //});
 
-  // Form data for the login modal
-  $scope.loginData = {};
+    app.service('textcodeService', function( $http ) {
+        var txtcodeSrv = this;
+        this.textcodes = [];
 
-  // Create the login modal that we will use later
-  $ionicModal.fromTemplateUrl('templates/login.html', {
-    scope: $scope
-  }).then(function(modal) {
-    $scope.modal = modal;
-  });
+        var addTextCode = function(newObj) {
+            textcodes.push(newObj);
+        };
 
-  // Triggered in the login modal to close it
-  $scope.closeLogin = function() {
-    $scope.modal.hide();
-  };
+        var getTextCodes = function() {
+            return this.textcodes;
+        };
 
-  // Open the login modal
-  $scope.login = function() {
-    $scope.modal.show();
-  };
+        var setTextCodes = function(txtcode) {
+            this.textcodes = txtcode;
+        };
 
-  // Perform the login action when the user submits the login form
-  $scope.doLogin = function() {
-    console.log('Doing login', $scope.loginData);
+        return {
+            addTextCode: addTextCode,
+            getTextCodes: getTextCodes,
+            setTextCodes: setTextCodes
+        };
 
-    // Simulate a login delay. Remove this and replace with your login
-    // code if using a login system
-    $timeout(function() {
-      $scope.closeLogin();
-    }, 1000);
-  };
+    });
+
+
+app.controller('AppCtrl', function($scope, $http, $ionicModal, $timeout ,textcodeService ) {
+
+    $scope.groups = [];
+
+    this.getDataJson = function() {
+        $http({
+            method: 'GET',
+            url: "js/textcode.json?ran=" + Math.random()
+        }).then(function successCallback(response) {
+            textcodeService.setTextCodes(response.data);
+            $scope.groups = response.data;
+            console.log($scope.groups);
+        }, function errorCallback(response) {
+            console.log("Failed to get data.json");
+        });
+    };
+
+
+    if (!$scope.groups.length)
+      $scope.groups = this.getDataJson();
+
+    $scope.loginData = {};
+
+    // Create the login modal that we will use later
+    $ionicModal.fromTemplateUrl('templates/login.html', {
+        scope: $scope
+    }).then(function(modal) {
+        $scope.modal = modal;
+    });
+
+    // Triggered in the login modal to close it
+    $scope.closeLogin = function() {
+        $scope.modal.hide();
+    };
+
+    // Open the login modal
+    $scope.login = function() {
+        $scope.modal.show();
+    };
+
+    // Perform the login action when the user submits the login form
+    $scope.doLogin = function() {
+        console.log('Doing login', $scope.loginData);
+
+        // Simulate a login delay. Remove this and replace with your login
+        // code if using a login system
+        $timeout(function() {
+            $scope.closeLogin();
+        }, 1000);
+    };
 })
 
-.controller('PlaylistsCtrl', function($scope) {
-  $scope.playlists = [
-    { title: 'Reggae', id: 1 },
-    { title: 'Chill', id: 2 },
-    { title: 'Dubstep', id: 3 },
-    { title: 'Indie', id: 4 },
-    { title: 'Rap', id: 5 },
-    { title: 'Cowbell', id: 6 }
-  ];
+app.controller('PlaylistsCtrl', function($scope) {
+    $scope.playlists = [{
+        title: 'Reggae',
+        id: 1
+    }, {
+        title: 'Chill',
+        id: 2
+    }, {
+        title: 'Dubstep',
+        id: 3
+    }, {
+        title: 'Indie',
+        id: 4
+    }, {
+        title: 'Rap',
+        id: 5
+    }, {
+        title: 'Cowbell',
+        id: 6
+    }];
 })
 
-.controller('PlaylistCtrl', function($scope, $stateParams) {
+app.controller('FrontPageCtrl', function($scope) {
+
+})
+
+
+app.controller('MobileCtrl', function($scope, $stateParams,textcodeService ) {
+  var ind = $stateParams.grouplistsId.substr(1);
+  var groups = textcodeService.getTextCodes(); 
+  $scope.group = groups[ind] ;
+   
 });
+
+app.controller('PlaylistCtrl', function($scope, $stateParams) {});
+
+})();

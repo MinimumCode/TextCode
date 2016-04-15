bd854dd4750bf2f88a8ac9e37d9fd99d959fe36a
inndee <inndeemercado@hotmail.com>
2016-04-15 21:39:16 +0800

    added in item option

--- textcode/www/js/controllers.js
+++ textcode/www/js/controllers.js
@@ -3,6 +3,51 @@
     var app = angular.module('textcode.controllers', []);
 
 
+    /*
+      Code from flavordaaave. Thanks dude this is awesome :)
+      https://forum.ionicframework.com/t/ion-option-button-in-list-toggle-by-icon-click-instead-of-swipe/9703/12
+    */
+    
+    app.directive('clickForOptions', ['$ionicGesture',
+        function($ionicGesture) {
+            return {
+                restrict: 'A',
+                link: function(scope, element, attrs) {
+                    element.bind('click', function(e) {
+                        e.stopPropagation();
+
+                        // Grab the content
+                        var content = element.parent()[0];
+
+                        // Grab the buttons and their width
+                        var buttons = element.parent().parent()[0].querySelector('.item-options');
+
+                        if (!buttons) {
+                            console.log('There are no option buttons');
+                            return;
+                        }
+                        var buttonsWidth = buttons.offsetWidth;
+
+                        ionic.requestAnimationFrame(function() {
+                            content.style[ionic.CSS.TRANSITION] = 'all ease-out .25s';
+
+                            if (!buttons.classList.contains('invisible')) {
+                                console.log('close');
+                                content.style[ionic.CSS.TRANSFORM] = '';
+                                setTimeout(function() {
+                                    buttons.classList.add('invisible');
+                                }, 250);
+                            } else {
+                                buttons.classList.remove('invisible');
+                                content.style[ionic.CSS.TRANSFORM] = 'translate3d(-' + buttonsWidth + 'px, 0, 0)';
+                            }
+                        });
+                    });
+                }
+            };
+        }
+    ]);
+
     app.service('textcodeService', function($http) {
         var txtcodeSrv = this;
         this.textcodes = [];
@@ -24,12 +69,12 @@
             this.textcodes = txtcode;
         };
 
-        var setId = function (type , ind ){
-            if ( type == "group")
+        var setId = function(type, ind) {
+            if (type == "group")
                 this.groupId = ind;
-            else if ( type == "sub")
+            else if (type == "sub")
                 this.subId = ind;
-            else if ( type == "item")
+            else if (type == "item")
                 this.item = ind;
         }
         return {
@@ -63,8 +108,8 @@
         if (!$scope.groups.length)
             $scope.groups = this.getDataJson();
 
-        $scope.setGroupId =function ( ind ){
-            textcodeService.setId("group",  ind  );
+        $scope.setGroupId = function(ind) {
+            textcodeService.setId("group", ind);
         };
 
         $scope.loginData = {};
@@ -126,19 +171,19 @@
 
 
     app.controller('MobileCtrl', function($scope, $stateParams, textcodeService) {
-        
+
         var textcodes = textcodeService.getTextCodes();
         $scope.mobile = textcodes[textcodeService.groupId];
 
-        $scope.subId = 0 ;
+        $scope.subId = 0;
 
-        $scope.setSubId = function( ind ){
+        $scope.setSubId = function(ind) {
             textcodeService.subId = ind;
         };
 
     });
 
-    app.controller('SubMobileCtrl', function($scope, $stateParams, textcodeService) {
+    app.controller('SubMobileCtrl', function($scope, $stateParams, $ionicListDelegate, textcodeService) {
 
         var textcodes = textcodeService.getTextCodes();
         $scope.group = textcodes[textcodeService.groupId].groups[textcodeService.subId];
